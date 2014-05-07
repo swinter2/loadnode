4ce874b100470a9822ab2008bd62eef5f188a172
Sam Winter <sam.winter@gmail.com>
2014-05-07 15:41:38 -0400

    sd7afoshlfkjahsdlkfjhasdfa

--- spawn.js
+++ spawn.js
@@ -1,17 +1,26 @@
 var spawn = require('child_process').spawn;
 
-console.log("Spawning load tester processes...")
-var worker1 = spawn('node', ['app.js', '20', '1', "http://pepco.demo.ekdev.silvertech.net/outage-center/"]);
-var worker2 = spawn('node', ['app.js', '20', '1', "http://pepco.demo.ekdev.silvertech.net/default.aspx"]);
-var worker3 = spawn('node', ['app.js', '20', '1', "http://pepco.demo.ekdev.silvertech.net/press-releases/"]);
-var worker4 = spawn('node', ['app.js', '20', '1', "http://pepco.demo.ekdev.silvertech.net/outage-center/"]);
-var worker5 = spawn('node', ['app.js', '20', '3', "http://pepco.demo.ekdev.silvertech.net/default.aspx"]);
+// var url = "http://pepco.local:8013/";
+// var url = "http://pepco.demo.ekdev.silvertech.net/";
+// var url = "http://pepco.demo.ekdev.silvertech.net/outage-center/";
+// var url = "http://pepco.local:8013/library/templates/Interior.aspx?pageid=740&site=pepco";
+// var url = "http://pepco.demo.ekdev.silvertech.net/outage-center/";
+// var url = "http://pepco.demo.ekdev.silvertech.net/press-releases/";
+// var url = "http://pepco.local:8013/library/templates/Interior.aspx?pageid=591&site=pepco";
 
-// var worker1 = spawn('node', ['app.js', '20', '1', "http://www.pepco.com/home/"]);
-// var worker2 = spawn('node', ['app.js', '20', '1', "http://www.pepco.com/home/"]);
-// var worker3 = spawn('node', ['app.js', '20', '1', "http://www.pepco.com/home/"]);
-// var worker4 = spawn('node', ['app.js', '20', '1', "http://www.pepco.com/home/"]);
-// var worker5 = spawn('node', ['app.js', '20', '3', "http://www.pepco.com/home/"]);
+var url = [
+    "http://pepco.demo.ekdev.silvertech.net/",
+    "http://pepco.demo.ekdev.silvertech.net/press-releases/",
+    "http://pepco.demo.ekdev.silvertech.net/outage-center/"
+];
+
+console.log("Spawning load tester processes...")
+
+var worker1 = spawn('node', ['app.js', '20', '1', url[0]]);
+var worker2 = spawn('node', ['app.js', '20', '1', url[1]]);
+var worker3 = spawn('node', ['app.js', '20', '1', url[2]]);
+var worker4 = spawn('node', ['app.js', '20', '1', url[0]]);
+var worker5 = spawn('node', ['app.js', '20', '3', url[1]]);
 
 worker1.stdout.on("data", function (data) {
     console.log("worker-1| " + data);
