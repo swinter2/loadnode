73f1d3626fdeff7aadbf2d162fdc43a74f857ac1
Sam Winter <s.winter@snhu.edu>
2017-02-03 10:56:54 -0500

    Refactored spawn.js to read from command line args.

--- spawn.js
+++ spawn.js
@@ -1,6 +1,17 @@
 var spawn = require('child_process').spawn;
 
-var host = 'http://localhost:80';
+var config = {
+    daemonCount: 1, // the number of daemons that should hit the site.
+    idleTime: 1, // how long a daemon should wait before hitting the site again in seconds
+    url: ""
+};
+
+var args = process.argv.slice(2);
+config.daemonCount = args[0] || config.daemonCount;
+config.idleTime = args[1] || config.idleTime;
+config.url = args[2] || config.url;
+
+var host = config.url;
 var paths = [
     "/"
     // "/about-us/",
@@ -14,19 +25,21 @@ for(var i in paths) {
 console.log("Spawning load tester processes...")
 
 function daemons() {
-    return "" + 20;
+    return `${config.daemonCount}`;
 }
 function r(len) {
     len = len || url.length;
     return Math.floor(Math.random() * len);
 }
 function rs(max) {
+    return `${config.idleTime}`
     // return "1";
-    max = max || 3;
-    return "" + Math.floor((Math.random() * max) + 1);
+    // max = max || 3;
+    // return "" + Math.floor((Math.random() * max) + 1);
 }
 function getUrl() {
-    return url[r()];
+    return config.url
+    // return url[r()];
 }
 
 function makeWorker(id) {
